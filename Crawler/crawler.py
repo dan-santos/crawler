591a6d74bfc2eeedd085b4bb3684f24c8a11fe2c
Daniel <danielsantosv8@usp.br>
2020-01-14 12:07:27 -0300

    Separando palavras e indexando-as

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -5,6 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import nltk
+import re
+import pymysql
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -13,44 +16,41 @@ http = urllib3.PoolManager()
 links = list() #onde ficarão armzazenados todos os links da busca em sites de notícias
 tweets = list() #onde ficarão armazenados todos os tweets dos dois candidatos
 
-#sites que o crawler irá buscar (e atributos importantes para a captura de informações)
-#Em todos os sites o período de busca será 01/08/18 até 01/11/18
-sites = ['http://g1.globo.com/busca/?q=Fake+News+Intelig%C3%AAncia+Artificial+elei%C3%A7%C3%B5es+SP+2018+Jo%C3%A3o+D%C3%B3ria+M%C3%A1rcio+Fran%C3%A7a&page=1&order=recent&from=2018-08-01T00%3A00%3A00-0300&to=2018-11-01T23%3A59%3A59-0300&species=not%C3%ADcias',
+#Nas listas abaixo, ficarão armazenados apenas o conteúdo do tweet de cada candidato, para a análise mais perspicaz
+#das temáticas mais abordadas poor cada um
+tweetsJoaoDoria = list()
+tweetsMarcioFranca = list()
+
+pegarLinks() #Método que pegará todos os links de todos os sites            
+pegarTweets() #Método que pegará todos os tweets (Essa lista servirá apenas para armazenarmos todos os tweets em BD)
+
+indexarTematicas(tweetsJoaoDoria, 1) #O número é a PK do candidato, pois precisamos contabilizar as temáticas por candidato
+indexarTematicas(tweetsMarcioFranca, 2)
+
+def pegarLinks():
+    #sites que o crawler irá buscar (e atributos importantes para a captura de informações)
+    #Em todos os sites o período de busca será 01/08/18 até 01/11/18
+    sites = ['http://g1.globo.com/busca/?q=Fake+News+Intelig%C3%AAncia+Artificial+elei%C3%A7%C3%B5es+SP+2018+Jo%C3%A3o+D%C3%B3ria+M%C3%A1rcio+Fran%C3%A7a&page=1&order=recent&from=2018-08-01T00%3A00%3A00-0300&to=2018-11-01T23%3A59%3A59-0300&species=not%C3%ADcias',
          'https://busca.estadao.com.br/?tipo_conteudo=Not%C3%ADcias&quando=01%2F08%2F2018-01%2F11%2F2018&q=Jo%C3%A3o%20D%C3%B3ria%20M%C3%A1rcio%20Fran%C3%A7a%20Fake%20News%20&editoria%5B%5D=Pol%C3%ADtica&editoria%5B%5D=Geral',
          'https://search.folha.uol.com.br/search?q=Jo%C3%A3o+D%C3%B3ria+M%C3%A1rcio+Fran%C3%A7a+Fake+News+Elei%C3%A7%C3%B5es+SP+2018&periodo=personalizado&sd=01%2F08%2F2018&ed=01%2F11%2F2018&site=todos']
-botoes = ['.results__content:last-child > div > a', '.mais-itens:last-child > div > a', '.c-pagination__arrow:last-child > a']
-tagLinks = ['.widget--info__text-container:last-child > a', '.link-title', '.c-headline__content:last-child > a']
-
-#pegarLinks(sites, botoes, tagLinks) #Método que pegará todos os links de todos os sites
-
-#tiramos os links inicialmente alocados na lista para usá-las novamente procurando os tweets
-sites.clear()
-tagLinks.clear()
-sites = ['https://twitter.com/search?f=tweets&vertical=default&q=since%3A2018-08-01%20until%3A2018-11-01%20from%3Ajdoriajr&src=unkn',
-         'https://twitter.com/search?f=tweets&vertical=default&q=since%3A2018-08-01%20until%3A2018-11-01%20from%3Amarciofrancagov&src=unkn']
-
-tagLinks = ['.content', #Tweet inteiro
-            ' > div[class="js-tweet-text-container"] > p'] #Conteúdo escrito do tweet
-            
-pegarTweets(sites, tagLinks)
-
-def pegarLinks(site, botao, tagLink):
+    botoes = ['.results__content:last-child > div > a', '.mais-itens:last-child > div > a', '.c-pagination__arrow:last-child > a']
+    tagLinks = ['.widget--info__text-container:last-child > a', '.link-title', '.c-headline__content:last-child > a']
     #Inicializando navegador
     opts = webdriver.ChromeOptions()
     opts.add_argument('start-maximized')
     opts.add_argument('disable-infobars')
     driver = webdriver.Chrome(options=opts, executable_path='chromedriver.exe')
     
-    for i in range(len(site)): #Percorrera todos os sites
-        driver.get(site[i]) #Acessando o site
+    for i in range(len(sites)): #Percorrera todos os sites
+        driver.get(sites[i]) #Acessando o site
         click = 0 #Variável que contará quantas vezes o botão de "Ver mais" ou semelhante foi clicado
         while click < 4: #Se existir, o botão "Ver mais" será clicado 4 vezes, contabilizando assim 5xNúmero de links padrão de cada site
             try:
                 if i == 2: #O site da folha mostra apenas 25 por vez, dessa forma, a cada clique nesse site, temos que armazenar os links
-                    for pags in driver.find_elements(By.CSS_SELECTOR, tagLink[i]): 
+                    for pags in driver.find_elements(By.CSS_SELECTOR, tagLinks[i]): 
                         links.append(pags.get_attribute('href'))
                 
-                driver.execute_script('arguments[0].click();', WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, botao[i]))))#Localizando botão de "ver mais"
+                driver.execute_script('arguments[0].click();', WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, botoes[i]))))#Localizando botão de "ver mais"
                 time.sleep(2) #Tempo se 2 seg de espera para evitar erros de carregamento da página
                 click += 1 #incremento
             except: #Só cairá aqui se não existe mais botão de "Ver mais"
@@ -58,7 +58,7 @@ def pegarLinks(site, botao, tagLink):
         
         if i != 2: #O site da folha não precisa passar por aqui por já foi analizada no for anterior
             #Os sites G1 e Estadão mostram todos os resultados, por isso deixamos para fazer a coleta de links agora
-            for pags in driver.find_elements(By.CSS_SELECTOR, tagLink[i]): #Percorrerá todos os links para as notícias
+            for pags in driver.find_elements(By.CSS_SELECTOR, tagLinks[i]): #Percorrerá todos os links para as notícias
                 if('https:' in pags.get_attribute('href')):
                     links.append(pags.get_attribute('href'))
                 else: # Links do G1 não possuem o 'https:' no início, por isso adicionamos antes do link esse trecho
@@ -67,7 +67,12 @@ def pegarLinks(site, botao, tagLink):
     driver.close() # Ao acabar de pegar todos os links de todos os sites, o navegador fecha
 
 
-def pegarTweets(perfisTwitter, tagLink):
+def pegarTweets():
+    perfisTwitter = ['https://twitter.com/search?f=tweets&vertical=default&q=since%3A2018-08-01%20until%3A2018-11-01%20from%3Ajdoriajr&src=unkn',
+         'https://twitter.com/search?f=tweets&vertical=default&q=since%3A2018-08-01%20until%3A2018-11-01%20from%3Amarciofrancagov&src=unkn']
+
+    tagLinks = ['.content', #Tweet inteiro
+            ' > div[class="js-tweet-text-container"] > p'] #Conteúdo escrito do tweet
     opts = webdriver.ChromeOptions()
     opts.add_argument('start-maximized')
     opts.add_argument('disable-infobars')
@@ -91,7 +96,7 @@ def pegarTweets(perfisTwitter, tagLink):
                 break
                 
             
-        for tweet in driver.find_elements(By.CSS_SELECTOR, tagLink[0]): #pegar todos os tweets
+        for tweet in driver.find_elements(By.CSS_SELECTOR, tagLinks[0]): #pegar todos os tweets
             if i == 0: #Marcio França
                 if ' retweetou ' in tweet.text: #retweet
                     tweets.append(tweet.text[:10] + '(' + tweet.text[29:38] + '), em ' + tweet.text[39:56] + 
@@ -110,13 +115,64 @@ def pegarTweets(perfisTwitter, tagLink):
                                   ', tweetou em resposta a ' + tweet.text[tweet.text.index('@', 48):tweet.text.index('\n', 87)] + ': ') #pegando @ do perfil que o candidato está respondendo
                 else:
                     tweets.append(tweet.text[:13] + '(' + tweet.text[32:48] + '), em ' + tweet.text[49:66] + ', tweetou: ')
-                
         
-        for tweet in driver.find_elements(By.CSS_SELECTOR, tagLink[0]+tagLink[1]):
+        for tweet in driver.find_elements(By.CSS_SELECTOR, tagLinks[0]+tagLinks[1]):
             tweets[posicaoTweet] += tweet.text
             posicaoTweet += 1
-            
+            if i == 0:
+                tweetsJoaoDoria.append(tweet)
+            else:
+                tweetsMarcioFranca.append(tweet)
             
         if i == 1:
             driver.close()
+            
+            
+def indexarTematicas(listaTweets, candidato):
+    for tweet in listaTweets:
+        indexarPalavras(separaPalavras(tweet), candidato)
+            
+            
+def separaPalavras(texto): #Vai pegar o texto dos tweets para contabilizar a quantidade de palavras mais repetidas por cada candidato
+    stopWords = nltk.corpus.stopwords.words('portuguese')
+    stopWords.append('é') #"é" não é considerado uma stopword, por isso, adio=cionamos ela manualmente
+    splitter = re.compile('\\W+') #Pega todas as paçavras que tenham a-zA-z0-9_
+    listaPalavras = []
+    lista = [p for p in splitter.split(texto) if p != ''] #pegando todos os termos do tweet
+    
+    for palavra in lista: #Pegando palavras que não são stopwords e nem espaços em branco
+        if palavra.lower() not in stopWords and len(palavra) > 1:
+            listaPalavras.append(palavra.lower())
+    return listaPalavras
+
+
+def indexarPalavras(listaDePalavras, candidato): #Vai salvar em banco de dados palavras novas e contabilizar +1 palavras repetidas
+    for p in listaDePalavras:
+        idPalavra = palavraIndexada(p, candidato) #Se for existente, incrementará +1 e idPalavra passrá a ter o valor 1
+        if idPalavra == -1: #Se não tem a palavra no BD
+            idPalavra = inserePalavra(p, candidato)
+            
+            
+#verificando se a palavra existe no bd
+def palavraIndexada(palavra):
+    conexao = pymysql.connect(host='localhost', user='root', passwd='fsociety', db='webcrawler', use_unicode = True, charset = 'utf8mb4')
+    cursorUrl = conexao.cursor()
+    idPalavra = cursorUrl.execute(f'select ID_Tematica from Tematicas where Nome_Tematica = {palavra} and ID_Candidato = {candidato}').fetchone()[0]
+    if cursorUrl.rowcount > 0: #se retornou algum resultado
+        qtd = cursorUrl.execute(f'select Quantidade_Tematica from Tematicas where ID_Tematica = {idPalavra}').fetchone()[0]
+        cursorUrl.execute(f'insert into Tematicas (Quantidade_Tematica) values {qtd+1} where ID_Tematica = {idPalavra}')
+        return 1
+    cursorUrl.close()
+    conexao.close()
+    return -1
+
+
+def inserePalavra(palavra, candidato):
+    conexao = pymysql.connect(host='localhost', user='root', passwd='fsociety', db='webcrawler', use_unicode = True, charset = 'utf8mb4')
+    cursorUrl = conexao.cursor()
+    cursorUrl.execute(f'insert into Tematicas values ({candidato}, {palavra}, {1})') #FK do candidato, Nome, Quantidade
+    cursorUrl.close()
+    conexao.close()
+
+
                 
