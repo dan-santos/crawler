bb2f10f924062bf88ebdfe8c9384f03fbe596e0e
Daniel <danielsantosv8@usp.br>
2020-01-20 12:55:40 -0300

    Inclusão do site O Globo no crawle

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -14,27 +14,28 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 http = urllib3.PoolManager()
 
 links = list() #onde ficarão armzazenados todos os links da busca em sites de notícias
-tweets = list() #onde ficarão armazenados todos os tweets dos dois candidatos
+#tweets = list() #onde ficarão armazenados todos os tweets dos dois candidatos
 
 #Nas listas abaixo, ficarão armazenados apenas o conteúdo do tweet de cada candidato, para a análise mais perspicaz
 #das temáticas mais abordadas poor cada um
-tweetsJoaoDoria = list()
-tweetsMarcioFranca = list()
+#tweetsJoaoDoria = list()
+#tweetsMarcioFranca = list()
 
-#pegarLinks() #Método que pegará todos os links de todos os sites            
-pegarTweets() #Método que pegará todos os tweets (Essa lista servirá apenas para armazenarmos todos os tweets em BD)
+pegarLinks() #Método que pegará todos os links de todos os sites            
+#pegarTweets() #Método que pegará todos os tweets (Essa lista servirá apenas para armazenarmos todos os tweets em BD)
 
-indexarTematicas(tweetsJoaoDoria, 1) #O número é a PK do candidato, pois precisamos contabilizar as temáticas por candidato
-indexarTematicas(tweetsMarcioFranca, 2)
+#indexarTematicas(tweetsJoaoDoria, 1) #O número é a PK do candidato, pois precisamos contabilizar as temáticas por candidato
+#indexarTematicas(tweetsMarcioFranca, 2)
 
 def pegarLinks():
     #sites que o crawler irá buscar (e atributos importantes para a captura de informações)
     #Em todos os sites o período de busca será 01/08/18 até 01/11/18
     sites = ['http://g1.globo.com/busca/?q=Fake+News+Intelig%C3%AAncia+Artificial+elei%C3%A7%C3%B5es+SP+2018+Jo%C3%A3o+D%C3%B3ria+M%C3%A1rcio+Fran%C3%A7a&page=1&order=recent&from=2018-08-01T00%3A00%3A00-0300&to=2018-11-01T23%3A59%3A59-0300&species=not%C3%ADcias',
-         'https://busca.estadao.com.br/?tipo_conteudo=Not%C3%ADcias&quando=01%2F08%2F2018-01%2F11%2F2018&q=Jo%C3%A3o%20D%C3%B3ria%20M%C3%A1rcio%20Fran%C3%A7a%20Fake%20News%20&editoria%5B%5D=Pol%C3%ADtica&editoria%5B%5D=Geral',
-         'https://search.folha.uol.com.br/search?q=Jo%C3%A3o+D%C3%B3ria+M%C3%A1rcio+Fran%C3%A7a+Fake+News+Elei%C3%A7%C3%B5es+SP+2018&periodo=personalizado&sd=01%2F08%2F2018&ed=01%2F11%2F2018&site=todos']
-    botoes = ['.results__content:last-child > div > a', '.mais-itens:last-child > div > a', '.c-pagination__arrow:last-child > a']
-    tagLinks = ['.widget--info__text-container:last-child > a', '.link-title', '.c-headline__content:last-child > a']
+         'https://busca.estadao.com.br/?tipo_conteudo=Not%C3%ADcias&quando=01%2F08%2F2018-01%2F11%2F2018&q=Jo%C3%A3o%20D%C3%B3ria%20M%C3%A1rcio%20Fran%C3%A7a%20&editoria%5B%5D=Pol%C3%ADtica&editoria%5B%5D=Geral',
+         'https://search.folha.uol.com.br/search?q=Jo%C3%A3o+D%C3%B3ria+M%C3%A1rcio+Fran%C3%A7a+Fake+News+Elei%C3%A7%C3%B5es+SP+2018&periodo=personalizado&sd=01%2F08%2F2018&ed=01%2F11%2F2018&site=todos',
+         'https://oglobo.globo.com/busca/?q=Jo%C3%A3o+D%C3%B3ria+M%C3%A1rcio+Fran%C3%A7a+Fake+News&species=not%C3%ADcias&page=1']
+    botoes = ['.results__content:last-child > div > a', '.mais-itens:last-child > div > a', '.c-pagination__arrow:last-child > a', '.proximo']
+    tagLinks = ['.widget--info__text-container:last-child > a', '.link-title', '.c-headline__content:last-child > a', '.link-url']
     #Inicializando navegador
     opts = webdriver.ChromeOptions()
     opts.add_argument('start-maximized')
@@ -46,23 +47,22 @@ def pegarLinks():
         click = 0 #Variável que contará quantas vezes o botão de "Ver mais" ou semelhante foi clicado
         while click < 4: #Se existir, o botão "Ver mais" será clicado 4 vezes, contabilizando assim 5xNúmero de links padrão de cada site
             try:
-                if i == 2: #O site da folha mostra apenas 25 por vez, dessa forma, a cada clique nesse site, temos que armazenar os links
+                if i >= 2: #O site da folha e o globo mostra apenas 25 por vez, dessa forma, a cada clique nesse site, temos que armazenar os links
                     for pags in driver.find_elements(By.CSS_SELECTOR, tagLinks[i]): 
-                        links.append(pags.get_attribute('href'))
+                        if pags.get_attribute('href') not in links: #se o link ainda não existe dentro da lista
+                            links.append(pags.get_attribute('href'))
                 
                 driver.execute_script('arguments[0].click();', WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, botoes[i]))))#Localizando botão de "ver mais"
                 time.sleep(2) #Tempo se 2 seg de espera para evitar erros de carregamento da página
                 click += 1 #incremento
+                
             except: #Só cairá aqui se não existe mais botão de "Ver mais"
                 break #Sai do while
         
-        if i != 2: #O site da folha não precisa passar por aqui por já foi analizada no for anterior
+        if i < 2: #O site da folha não precisa passar por aqui por já foi analizada no for anterior
             #Os sites G1 e Estadão mostram todos os resultados, por isso deixamos para fazer a coleta de links agora
             for pags in driver.find_elements(By.CSS_SELECTOR, tagLinks[i]): #Percorrerá todos os links para as notícias
-                if('https:' in pags.get_attribute('href')):
-                    links.append(pags.get_attribute('href'))
-                else: # Links do G1 não possuem o 'https:' no início, por isso adicionamos antes do link esse trecho
-                    links.append('https:' + pags.get_attribute('href'))
+                links.append(pags.get_attribute('href'))
     
     driver.close() # Ao acabar de pegar todos os links de todos os sites, o navegador fecha
 
@@ -124,7 +124,7 @@ def pegarTweets():
             else:
                 tweetsMarcioFranca.append(tweet.text)
             
-        if i == 1:
+        if i == 2:
             driver.close()
             
             
