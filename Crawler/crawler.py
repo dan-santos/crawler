cff433a212413a0abd1986852ddd6c5d94154a9e
Daniel <danielsantosv8@usp.br>
2020-04-06 11:54:22 -0300

    Crawler update

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -71,59 +71,6 @@ def recuperarTweets():
     cursorUrl.close()
     conexao.close()
     
-
-def analizarEmocoes():
-    base = [('eu sou admirada por muitos','alegria'),
-        ('me sinto completamente amado','alegria'),
-        ('amar e maravilhoso','alegria'),
-        ('estou me sentindo muito animado novamente','alegria'),
-        ('eu estou muito bem hoje','alegria'),
-        ('que belo dia para dirigir um carro novo','alegria'),
-        ('o dia está muito bonito','alegria'),
-        ('estou contente com o resultado do teste que fiz no dia de ontem','alegria'),
-        ('o amor e lindo','alegria'),
-        ('nossa amizade e amor vai durar para sempre', 'alegria'),
-        ('estou amedrontado', 'medo'),
-        ('ele esta me ameacando a dias', 'medo'),
-        ('isso me deixa apavorada', 'medo'),
-        ('este lugar e apavorante', 'medo'),
-        ('se perdermos outro jogo seremos eliminados e isso me deixa com pavor', 'medo'),
-        ('tome cuidado com o lobisomem', 'medo'),
-        ('se eles descobrirem estamos encrencados', 'medo'),
-        ('estou tremendo de medo', 'medo'),
-        ('eu tenho muito medo dele', 'medo'),
-        ('estou com medo do resultado dos meus testes', 'medo')]
-    
-
-def buscaPalavras(frases):
-    todasPalavras = list()
-    for(palavras, emocao) in frases:
-        todasPalavras.extend(palavras)
-    return todasPalavras
-
-
-def buscaFrequencia(palavras):
-    palavras = nltk.FreqDist(palavras)
-    return palavras
-
-palavras = []
-frequencia = buscaFrequencia(palavras)
-frequencia = frequencia.most_common(60) #pega as 60 palavras mais usadas na frase
-
-def buscaPalavrasUnicas(freq):
-    f = freq.keys()
-    return f
-
-palavrasUnicas = buscaPalavrasUnicas(frequencia)
-
-def extratorPalavras(palavras):
-    caracteristicas = {}
-    for palvrs in palavrasUnicas:
-        caracteristicas['%s' % palvrs] = palvrs in palavras
-    return caracteristicas
-
-baseCompleta = nltk.classify.apply_features(extratorPalavras, tweet)
-    
     
 def formatar(texto):
     texto = texto.strip()
